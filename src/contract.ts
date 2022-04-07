c960df665753db6d5766e287c7cd6ec23c7c3c74
z <alex@dwfe.ru>
2022-04-07 10:49:46 +0300

    .

--- src/contract.ts
+++ src/contract.ts
@@ -1,9 +1,23 @@
 import {Type} from '@do-while-for-each/common';
 
-export type TProvider = ICommonProvider;
+export type TProvider = ICommonEntry;
 
-export interface ICommonProvider {
+export interface ICommonEntry {
   provide: any;
   useClass?: Type<any>;
   deps?: any[];
 }
+
+class Duck {
+  constructor(public message?: string) {
+  }
+}
+
+new Map<TProvider, any[]>([
+  [
+    {provide: Duck, useClass: Duck}, []
+  ],
+  [
+    {provide: Duck, deps: ['quack!']}, []
+  ]
+])
