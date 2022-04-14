b5f97548bbc820aeb2f30190537590c75f172a35
z <alex@dwfe.ru>
2022-04-15 01:40:20 +0300

    .

--- src/provider/provider.ts
+++ src/provider/provider.ts
@@ -4,16 +4,22 @@ export class Provider {
 
   private registry = new Registry();
 
-  get(provide: any) {
+  get<TResult = any>(provide: any): TResult | undefined {
     const entries = this.registry.get(provide);
     if (!entries)
       return;
     for (const entry of entries) {
+      let {deps} = entry;
       switch (entry.result) {
         case 'value':
           return entry.useValue;
         case 'class-instance':
-
+          const useClass = entry.useClass as any;
+          if (!deps) {
+            // TODO взять из metadata
+            deps = [];
+          }
+          return new useClass(...deps) as TResult;
         // return instance;
         case 'factory-result':
           return (entry.useFactory as Function)();
