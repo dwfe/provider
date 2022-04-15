3ad9806b00820198bc2890eaea86605b8b25f364
z <alex@dwfe.ru>
2022-04-15 13:44:41 +0300

    .

--- src/provider/provider.ts
+++ src/provider/provider.ts
@@ -4,10 +4,11 @@ export class Provider {
 
   private registry = new Registry();
 
-  get<TResult = any>(provide: any, deps?: any[]): TResult | undefined {
+  get<TResult = any>(provide: any, deps?: any[]): TResult | TResult[] | undefined {
     const entries = this.registry.get(provide, deps);
     if (!entries)
       return;
+    const result = [];
     for (const entry of entries) {
       if (entry.result === 'value')
         return entry.useValue;
@@ -19,15 +20,23 @@ export class Provider {
       }
       switch (entry.result) {
         case 'class-instance':
-          const useClass = entry.useClass as any;
-          return new useClass(...deps) as TResult;
+          result.push(new (entry.useClass as any)(...deps) as TResult);
+          break;
         case 'factory-result':
-          return (entry.useFactory as Function)();
+          result.push((entry.useFactory as Function)(...deps) as TResult);
+          break;
         default:
           throw new Error(`Unknown entry result "${entry.result}"`);
       }
     }
-
+    switch (result.length) {
+      case 0:
+        return undefined;
+      case 1:
+        return result[0];
+      default:
+        return result;
+    }
   }
 
 }
