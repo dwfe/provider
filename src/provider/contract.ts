c6b2dbb16d3aa8d91b790f1a2f2d2fc181e1e377
z <alex@dwfe.ru>
2022-04-15 17:20:45 +0300

    .

--- src/provider/contract.ts
+++ src/provider/contract.ts
@@ -1,4 +1,16 @@
 export interface IProviderGetOpt {
+
   deps?: any[]; // a second-level filter applied to the result of getting registry entries
-  primitiveCanBeResult?: boolean; // e.g. {provide: User, deps: ['John']}
+
+  /**
+   * E.g.
+   *   {provide: User, deps: ['John']}
+   * when
+   *   {provide: 'John', ...}
+   * is not registered in the registry.
+   * As a result, the instance will be created like this:
+   *   new User('John')
+   */
+  primitiveCanBeResult?: boolean;
+
 }
