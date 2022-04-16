c490bc0805a5550913f5ff163a12d2b5382f442e
z <alex@dwfe.ru>
2022-04-16 21:20:44 +0300

    .

--- src/__tests__/provider/get.test.ts
+++ src/__tests__/provider/get.test.ts
@@ -1,7 +1,8 @@
 import {describe, expect} from '@jest/globals';
 import {L10nService} from '../abc/l10n.service';
-import {getLang} from '../abc/getLang'
+import {Duck, Turkey} from '../abc/bird'
 import {Provider} from '../../provider';
+import {getLang} from '../abc/getLang'
 import {User} from '../abc/user';
 
 //region Support
@@ -10,6 +11,8 @@ const provider = Provider.of([
   {provide: User, deps: [L10nService, 'Naruto']},
   {provide: L10nService, deps: ['ja']},
   {provide: 'lang-factory', useFactory: getLang, deps: [User]},
+  {provide: Turkey, useClass: Turkey},
+  {provide: Duck, useValue: new Duck()},
 ]);
 
 //endregion Support
@@ -17,20 +20,31 @@ const provider = Provider.of([
 describe('Provider.getAll', () => {
 
   test('primitive can be result', () => {
-    const user = provider.getOnlyOne<User>(User);
-    expect(user).toBeTruthy();
-    expect(user.l10nService).not.toBe(undefined);
-    expect(user).toHaveProperty('lang', 'ja');
-    expect(user).toHaveProperty('name', 'Naruto');
+    const value = provider.getOnlyOne<User>(User);
+    expect(value).toBeTruthy();
+    expect(value instanceof User).toBe(true);
+    expect(value.l10nService).not.toBe(undefined);
+    expect(value).toHaveProperty('lang', 'ja');
+    expect(value).toHaveProperty('name', 'Naruto');
   });
 
   test('useClass', () => {
-
+    const value = provider.getOnlyOne<Turkey>(Turkey);
+    expect(value).toBeTruthy();
+    expect(value instanceof Turkey).toBe(true);
+    expect(value.sound()).toBe('ololo!');
   });
 
   test('useFactory', () => {
     const factoryResult = provider.getOnlyOne<string>('lang-factory');
     expect(factoryResult).toBe('ja');
+  });
+
+  test('useValue', () => {
+    const value = provider.getOnlyOne<Duck>(Duck);
+    expect(value).toBeTruthy();
+    expect(value instanceof Duck).toBe(true);
+    expect(value.sound()).toBe('quack!');
   });
 
   // test('', () => {
