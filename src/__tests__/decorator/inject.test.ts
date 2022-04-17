b6cfe11aad85a16293b02e5efe64c56383342af7
z <alex@dwfe.ru>
2022-04-17 23:41:52 +0300

    .

--- src/__tests__/decorator/inject.test.ts
+++ src/__tests__/decorator/inject.test.ts
@@ -1,8 +1,8 @@
 import {describe} from '@jest/globals'
 import 'reflect-metadata';
-import {inject, injectable} from '../../decorator'
+import {inject, injectable, single} from '../../decorator'
 import {checkMetadata} from './injectable.test'
-import {provider} from '../../provider'
+import {Provider} from '../../provider'
 
 //region Support
 
@@ -11,14 +11,14 @@ import {provider} from '../../provider'
 
 describe('Decorator.inject', () => {
 
-  test('', () => {
+  test('@injectable', () => {
     @injectable()
     class A1 {
       constructor() {
       }
     }
 
-    @injectable({isOnlyOne: true})
+    @injectable()
     class A2 {
       constructor(public id = 90,
                   public a1: A1,
@@ -28,8 +28,34 @@ describe('Decorator.inject', () => {
     }
 
     checkMetadata(A1, {}, []);
+    checkMetadata(A2, {ctorParams: {[2]: 'hello'}}, [Object, A1, Object, Boolean]);
+    const a2 = Provider.of().getOnlyOne<A2>(A2);
+    expect(a2).toBeInstanceOf(A2);
+    expect(a2).toHaveProperty('name', 'hello');
+    expect(a2.a1).toBeInstanceOf(A1);
+    expect(a2).toHaveProperty('id', 90);
+    expect(a2).toHaveProperty('type', undefined);
+  });
+
+  test('@single', () => {
+    @single
+    class A1 {
+      constructor() {
+      }
+    }
+
+    @single
+    class A2 {
+      constructor(public id = 90,
+                  public a1: A1,
+                  @inject('hello') public name = 'Naruto',
+                  public type: boolean) {
+      }
+    }
+
+    checkMetadata(A1, {isOnlyOne: true}, []);
     checkMetadata(A2, {isOnlyOne: true, ctorParams: {[2]: 'hello'}}, [Object, A1, Object, Boolean]);
-    const a2 = provider.getOnlyOne<A2>(A2);
+    const a2 = Provider.of().getOnlyOne<A2>(A2);
     expect(a2).toBeInstanceOf(A2);
     expect(a2).toHaveProperty('name', 'hello');
     expect(a2.a1).toBeInstanceOf(A1);
