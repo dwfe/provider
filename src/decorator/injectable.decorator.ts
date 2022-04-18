4ac34bf7c6bf554858d0bdf1e155d80cc5fe013c
z <alex@dwfe.ru>
2022-04-18 14:10:21 +0300

    .

--- src/decorator/injectable.decorator.ts
+++ src/decorator/injectable.decorator.ts
@@ -2,12 +2,10 @@ import {IProviderMetadata, PROVIDER_METADATA_KEY} from '../provider'
 
 export function injectable(metadata: IProviderMetadata = {}) {
   return (target: object) => {
-    const existedMetadata = Reflect.getMetadata(PROVIDER_METADATA_KEY, target);
-    if (existedMetadata)
-      metadata = {
-        ...existedMetadata,
-        ...metadata,
-      };
+    metadata = {
+      ...(Reflect.getMetadata(PROVIDER_METADATA_KEY, target) || {}),
+      ...metadata,
+    };
     Reflect.defineMetadata(PROVIDER_METADATA_KEY, metadata, target);
   };
 }
